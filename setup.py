48be42d3aa6d4f08bd05af1be047581461cd992f
Felix Panozzo <fpanozzo@gmail.com>
2014-11-20 12:44:57 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Racconto',
-    version='0.0.1',
+    version='0.0.2',
     # FIXME: walk directories instead of hard coding package names
     packages=['racconto','racconto.hooks'],
     license='MIT',
