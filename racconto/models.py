dcf4cdf944c0ecfabc83a8ccd8009c1247675d0e
Gunnar Hansson <gunnar@avidity.se>
2015-01-15 22:37:35 -0200

    Adding section and meta classes, and a pagecontent class to organise them

--- racconto/models.py
+++ racconto/models.py
@@ -3,28 +3,30 @@ from functools import total_ordering
 from racconto.settings_manager import SettingsManager as SETTINGS
 
 #
-# Content containers (Post and Page)
+# Page containers (Post and Page)
 #
 
-class ContentBase(object):
+class PageBase(object):
     """ Base class for compiled content """
     def __init__(self, options):
         self.title = options["title"]
         self.template = options["template"]
-        self.body = options["body"]
+        self.content = PageContent(options["content"], options["meta"])
+        self.meta = self.content.meta   # FIXME: Not sure about this interface
+
         if "slug" in options.keys():
             self.slug = options["slug"]
         else:
             self.slug = options["filepath"].split("/")[-1:][0].split(".")[0:-1][0]
 
         self.template_parameters = {"title": self.title,
-                                    "body": self.body,
+                                    "content": self.content
                                    }
         # Add config variables to template parameters
         for param in options["config"]:
             self.template_parameters[param] = options["config"][param]
 
-class Page(ContentBase):
+class Page(PageBase):
     def __init__(self, options):
         super(Page, self).__init__(options)
         self.filepath = "%s" % self.slug
@@ -33,8 +35,9 @@ class Page(ContentBase):
     def __str__(self):
         return "%s" % self.title
 
+
 @total_ordering # Auto generate missing ordering methods
-class Post(ContentBase):
+class Post(PageBase):
     """
     Container for posts
     Implements __lt__ for sorting posts on their published date
@@ -46,7 +49,8 @@ class Post(ContentBase):
         super(Post, self).__init__(options)
         self.slug = self.slug[11:] # Truncate date
         self.date = options["date"]
-        self.template = options["config"].get("template", SETTINGS.get('PAGE_TEMPLATE'))
+        # FIXME: Fix this
+        self.template = options["template"] # , SETTINGS.get('PAGE_TEMPLATE'))
         self.template_parameters["date"] = self.date
         self.filepath = "%s/%s" % (str(self.date.strftime('%Y-%m-%d')).replace('-','/'),
             self.slug,
@@ -63,3 +67,44 @@ class Post(ContentBase):
 
     def __lt__(self, other):
         return self.date > other.date
+
+
+
+#
+# Content containers (Meta and Section)
+#
+
+class PageContent(object):
+    def __init__(self, sections, page_meta=None):
+        self._iter_sections = iter(sections)
+        self.sections = dict((s.name, s) for s in sections)
+        self.meta = page_meta
+
+    def next(self):
+        next(self._iter_sections)
+
+    def __iter__(self):
+        return self._iter_sections
+
+    def __len__(self):
+        return len(self.sections)
+
+    def __str__(self):
+        return "\n".join(self.sections)
+
+
+class Section(object):
+    def __init__(self, name, content, meta=None):
+        self.name = name
+        self.meta = meta
+        self.content = content
+
+    def __repr__(self):
+        return "Section('%s')" % (self.name)
+
+    def __str__(self):
+        return "%s" % (self.content)
+
+class Meta(object):
+    def __init__(self, options):
+        self.options = options
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,10 +1,10 @@
 import unittest
 import datetime
+import copy
 
 from racconto.models import *
 
-class TestContentBase(unittest.TestCase):
-
+class TestPageBase(unittest.TestCase):
     def setUp(self):
         self.options = {
             "title": "some title",
@@ -13,7 +13,7 @@ class TestContentBase(unittest.TestCase):
             "filepath": "tests/a-file-name.md",
             "config": ""
             }
-        self.content_base = ContentBase(self.options)
+        self.content_base = PageBase(self.options)
 
     def tearDown(self):
         del self.content_base
@@ -28,11 +28,10 @@ class TestContentBase(unittest.TestCase):
     def test_custom_slug(self):
         other_options = self.options.copy()
         other_options["slug"] = "my-custom-slug"
-        other_content_base = ContentBase(other_options)
+        other_content_base = PageBase(other_options)
         self.assertEqual(other_content_base.slug, "my-custom-slug")
 
 class TestPost(unittest.TestCase):
-
     def setUp(self):
         self.options = {
             "title": "Title of a testing post",
@@ -64,5 +63,54 @@ class TestPost(unittest.TestCase):
         self.assertEqual(post_list[0].date, in_4_days)
 
 
+class TestSection(unittest.TestCase):
+    def setUp(self):
+        self.the_content = "This is the content"
+        self.the_name = "my section"
+
+        self.section = Section(self.the_name, self.the_content)
+
+    def tearDown(self):
+        pass
+
+    def test_name_option(self):
+        self.assertEqual(self.section.name, self.the_name)
+
+    def test_content_option(self):
+        self.assertEqual(self.section.content, self.the_content)
+
+    def test_stringifies_to_content(self):
+        self.assertEqual("%s" % (self.section), self.the_content)
+
+class TestPageContent(unittest.TestCase):
+    def setUp(self):
+        self.the_meta = Meta({})
+        self.the_sections = [
+            Section("section-1", "This is section 1"),
+            Section("section-2", "This movie is stupid")
+        ]
+
+        self.page_content = PageContent(self.the_sections, self.the_meta)
+
+    def tearDown(self):
+        pass
+
+    def test_iterate_sections(self):
+        s = iter(self.the_sections)
+        for section in self.page_content:
+            self.assertEqual(section, next(s))
+
+    def test_sections_length(self):
+        self.assertEqual(len(self.page_content), 2)
+
+    def test_sections_is_dict_keyed_on_name(self):
+        self.assertEqual(self.page_content.sections['section-1'], self.the_sections[0])
+        self.assertEqual(self.page_content.sections['section-2'], self.the_sections[1])
+
+    def test_meta_property(self):
+        self.assertEqual(self.page_content.meta, self.the_meta)
+
+
 if __name__ == '__main__':
     unittest.main()
+
